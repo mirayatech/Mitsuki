1162f755cc277df22bd045be9d4b577ff1a64380
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 10:03:58 +0200

    🛸 adding eventistener to navigationbar links

--- script/utilities.js
+++ script/utilities.js
@@ -1,9 +1,12 @@
 // Selectors 
 const introductionLink = document.querySelector(".introduction-link");
 const introductionPage = document.querySelector(".introduction");
-const navbarLinks = document.querySelector(".item-links");
+const navLinks = document.querySelector(".item-links");
 const boardLink = document.querySelector(".boards-link");
 const boardPage = document.querySelector(".board-page");
 const noteLink = document.querySelector(".notes-link");
 const notePage = document.querySelector(".note-page");
+
+// Event Listener
+navLinks.addEventListener("click", selectNavLink);
 
