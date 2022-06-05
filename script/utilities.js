0dd96d453c3be98223062482ecee58c63ad36517
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 12:38:15 +0200

    🛸 js: add actice class if we press on one of the navlinks

--- script/utilities.js
+++ script/utilities.js
@@ -1,12 +1,17 @@
 // Selectors 
 const introductionLink = document.querySelector(".introduction-link");
 const introductionPage = document.querySelector(".introduction");
-const navLinks = document.querySelector(".item-links");
+const navLinks = document.querySelectorAll(".item-links");
 const boardLink = document.querySelector(".boards-link");
 const boardPage = document.querySelector(".board-page");
 const noteLink = document.querySelector(".notes-link");
 const notePage = document.querySelector(".note-page");
 
-// Event Listener
-navLinks.addEventListener("click", selectNavLink);
+// 
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.querySelector(".item-links.active").classList.remove("active")
+        link.classList.add("active")
+    })
+})
 
