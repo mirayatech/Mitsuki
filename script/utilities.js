fd924025df32cd7abd022fc34367f2dbd8dfa2b7
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 13:48:39 +0200

    🧭  create function for nav-links

--- script/utilities.js
+++ script/utilities.js
@@ -12,6 +12,30 @@ navLinks.forEach((link) => {
     link.addEventListener("click", () => {
         document.querySelector(".item-links.active").classList.remove("active")
         link.classList.add("active")
+        showPages(link);
     })
 })
 
+function showPages(link) {
+    // If we click on Introduction link
+    if (link === introductionLink) {
+        boardPage.classList.remove("show");
+        notePage.classList.remove('show')
+        introductionPage.classList.remove("hide")
+    }
+
+    else if (link === boardLink) {
+        boardPage.classList.add("show");
+        notePage.classList.remove('show')
+        introductionPage.classList.add("hide")
+
+
+    }
+
+    else if (link === noteLink) {
+        notePage.classList.add("show");
+        boardPage.classList.remove('show')
+        introductionPage.classList.add("hide")
+
+    }
+}
