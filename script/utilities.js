0cc118f1c118640fc89c67a44e665dffc024cff3
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 10:01:46 +0200

    📍 selecting one more required selector

--- script/utilities.js
+++ script/utilities.js
@@ -1,7 +1,9 @@
 // Selectors 
 const introductionLink = document.querySelector(".introduction-link");
 const introductionPage = document.querySelector(".introduction");
+const navbarLinks = document.querySelector(".item-links");
 const boardLink = document.querySelector(".boards-link");
 const boardPage = document.querySelector(".board-page");
 const noteLink = document.querySelector(".notes-link");
 const notePage = document.querySelector(".note-page");
+
