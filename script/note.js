d52ea74f3c88b19514cf548b1166e828bfa9415f
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 14:16:20 +0200

    💡 immediately function: show popup box

--- script/note.js
+++ script/note.js
@@ -2,3 +2,8 @@
 const addNote = document.querySelector(".circle");
 const addNotePopup = document.querySelector(".note-page .popup-box");
 
+// Show popup
+addNote.addEventListener('click', () => {
+    addNotePopup.classList.add("show")
+});
+
