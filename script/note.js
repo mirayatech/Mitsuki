1b8066df1faaaba2056c41b80bf5c628c04b9d32
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-05 14:24:25 +0200

    💡 immediately function: close the popupbox

--- script/note.js
+++ script/note.js
@@ -1,5 +1,6 @@
 // Selectors 
 const addNote = document.querySelector(".circle");
+const closeIcon = document.querySelector("header i")
 const addNotePopup = document.querySelector(".note-page .popup-box");
 
 // Show popup
@@ -7,3 +8,6 @@ addNote.addEventListener('click', () => {
     addNotePopup.classList.add("show")
 });
 
+closeIcon.addEventListener("click", () => {
+    addNotePopup.classList.remove("show")
+})
