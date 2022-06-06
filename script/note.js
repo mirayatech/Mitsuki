91b9a0f94c850f3c93d9fd02195eb28a51ce6b8a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-06 09:56:31 +0200

    💡 Note info save in local storage and format document

--- script/note.js
+++ script/note.js
@@ -1,13 +1,58 @@
-// Selectors 
-const addNote = document.querySelector(".circle");
-const closeIcon = document.querySelector("header i")
-const addNotePopup = document.querySelector(".note-page .popup-box");
+const addNote = document.querySelector('.circle')
+const closeIcon = document.querySelector('header i')
+const addNoteBtn = document.querySelector('.note-page .popup-btn')
+const addNotePopup = document.querySelector('.note-page .popup-box')
+const titleTag = document.querySelector('.note-page #title-description')
+const descTag = document.querySelector('.note-page #description-input')
+
+const months = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+const notes = JSON.parse(localStorage.getItem('notes') || '[]')
 
 // Show popup
 addNote.addEventListener('click', () => {
-    addNotePopup.classList.add("show")
-});
+    addNotePopup.classList.add('show')
+})
 
-closeIcon.addEventListener("click", () => {
-    addNotePopup.classList.remove("show")
+// close the popup
+closeIcon.addEventListener('click', () => {
+    addNotePopup.classList.remove('show')
+})
+
+// Add Note information to Local storage
+addNoteBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let noteTitle = titleTag.value
+    let noteDesc = descTag.value
+    // Get the date
+    let dateObj = new Date()
+    let month = months[dateObj.getMonth()]
+    let day = dateObj.getDate()
+    let year = dateObj.getFullYear()
+    let noteInfo = {
+        title: noteTitle,
+        description: noteDesc,
+        date: `${month} ${day}, ${year}`,
+    }
+
+    if (noteTitle || noteDesc) {
+        console.log(noteInfo)
+    }
+
+    notes.push(noteInfo)
+
+    localStorage.setItem('notes', JSON.stringify(notes))
 })
