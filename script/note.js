80893afce45b4946320a1d1bc4ab785f19efc5af
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-07 07:39:52 +0200

    💡 Edit note function

--- script/note.js
+++ script/note.js
@@ -2,6 +2,7 @@ const addNote = document.querySelector('.circle')
 const addBox = document.querySelector('.add-note')
 const closeIcon = document.querySelector('header i')
 const addNoteBtn = document.querySelector('.note-page .popup-btn')
+const popupTitle = document.querySelector('.note-page .popup-title')
 const addNotePopup = document.querySelector('.note-page .popup-box')
 const titleTag = document.querySelector('.note-page #title-description')
 const descTag = document.querySelector('.note-page #description-input')
@@ -23,8 +24,13 @@ const months = [
 
 const notes = JSON.parse(localStorage.getItem('notes') || '[]')
 
+let isUpdate = false,
+    updateId
+
 // Show popup
 addNote.addEventListener('click', () => {
+    isUpdate = false
+    titleTag.focus()
     addNotePopup.classList.add('show')
 })
 
@@ -50,7 +56,7 @@ function showNotes() {
                             <div class="settings">
                                 <i onclick="showMenu(this)" class="fa-solid fa-ellipsis"></i>
                             <ul class="settings-menu">
-                                <li class="edit-btn"><i class="fa-solid fa-pen"> </i> Edit </li>
+                                <li onclick="updateNote(${index}, '${note.title}', '${note.description}')" class="edit-btn"><i class="fa-solid fa-pen"> </i> Edit </li>
                                 <li onclick="deleteNote(${index})" class="delete-btn"><i class="fa-solid fa-trash"> </i> Delete </li>
                             </ul>
                         </div>
@@ -76,6 +82,18 @@ function showMenu(element) {
         }
     })
 }
+
+function updateNote(noteId, title, desc) {
+    isUpdate = true
+    updateId = noteId
+    addNote.click()
+    titleTag.value = title
+    descTag.value = desc
+    popupTitle.innerText = 'Update Note'
+    addNoteBtn.innerText = 'Update note'
+    console.log(noteId, title, desc)
+}
+
 // Add Note information to Local storage
 addNoteBtn.addEventListener('click', (event) => {
     event.preventDefault()
@@ -92,10 +110,14 @@ addNoteBtn.addEventListener('click', (event) => {
         date: `${month} ${day}, ${year}`,
     }
 
-    notes.push(noteInfo)
+    if (!isUpdate) {
+        notes.push(noteInfo)
+    } else {
+        isUpdate = false
+        notes[updateId] = noteInfo // updating specifed note
+    }
 
     localStorage.setItem('notes', JSON.stringify(notes))
-
     closeIcon.click()
     showNotes()
 })
