fc848948ccb12a064d950475b72579ada68976c0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-08 07:18:34 +0200

    🐛 FIX: cange class names so js understand what is what

--- script/board.js
+++ script/board.js
@@ -1,17 +1,22 @@
-const addColumn = document.querySelector('.add-column')
+const addColumnDiv = document.querySelector('.add-column')
 const addColumnBtn = document.querySelector('.add-column-btn')
 const entitleButtonDiv = document.querySelector('.entitle-column')
-const entitleColumnInput = document.querySelector('entitle-column-input')
-const addBtn = document.querySelector('.add-column-btn')
+const entitleColumnInput = document.getElementById('entitle-column-input')
+const addBtn = document.querySelector('.add-btn')
 const cancelBtn = document.querySelector('.cancle-column-btn')
 
-addColumn.addEventListener('click', () => {
+addColumnDiv.addEventListener('click', () => {
     entitleButtonDiv.classList.add('show')
-    addColumn.classList.add('hide')
+    addColumnDiv.classList.add('hide')
 
 })
 
 cancelBtn.addEventListener('click', () => {
     entitleButtonDiv.classList.remove('show')
-    addColumn.classList.remove('hide')
+    addColumnDiv.classList.remove('hide')
+})
+
+addBtn.addEventListener('click', () => {
+    let columnTitle = entitleColumnInput.value;
+    console.log(columnTitle)
 })
