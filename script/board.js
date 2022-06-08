60ee539a1ace7253d427ac61efd6ad6e3e019e9d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-08 18:42:49 +0200

    💡  saving column to local storage and show them on page

--- script/board.js
+++ script/board.js
@@ -5,7 +5,11 @@ const entitleColumnInput = document.getElementById('entitle-column-input')
 const addBtn = document.querySelector('.add-btn')
 const cancelBtn = document.querySelector('.cancle-column-btn')
 
-addColumnDiv.addEventListener('click', () => {
+const boards = JSON.parse(localStorage.getItem('board') || '[]')
+
+addColumnBtn.addEventListener('click', () => {
+    entitleColumnInput.value = ''
+    entitleColumnInput.focus()
     entitleButtonDiv.classList.add('show')
     addColumnDiv.classList.add('hide')
 
@@ -16,7 +20,53 @@ cancelBtn.addEventListener('click', () => {
     addColumnDiv.classList.remove('hide')
 })
 
+function showColumn() {
+    boards.forEach(column => {
+        let liTag = `<div class="add-card">
+        <h3 class="column-title">${column.column}</h3>
+        <button class="add-card-btn"><i class="fa-solid fa-plus"></i> New Card</button>
+        <ul class="card-list">
+            <li class="column-card">
+                <div class="card-details">
+                    <h3 class="title">Card Title</h3>
+                    <p>Lorem ipsum, dolor sit amet consectur adipisig boli somin kol elit.
+                    </p>
+                </div>
+                <div class="line"></div>
+                <div class="bottom-details">
+                    <span class="date">June 4, 2022</span>
+                    <div class="settings"><i class="fa-solid fa-ellipsis" onclick="showMenu(this)"></i>
+                        <ul class="settings-menu">
+                            <li class="edit-btn"> <i class="fa-solid fa-pen"> </i> Edit </li>
+                            <li class="delete-btn"> <i class="fa-solid fa-trash"> </i> Delete </li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>`
+
+        addColumnDiv.insertAdjacentHTML('afterend', liTag)
+
+    });
+}
+
 addBtn.addEventListener('click', () => {
     let columnTitle = entitleColumnInput.value;
-    console.log(columnTitle)
+
+    if (columnTitle) {
+        let boardInfo = {
+            column: columnTitle,
+            card: {
+                title: '',
+                description: '',
+                date: '',
+            }
+        }
+
+        boards.push(boardInfo)
+        localStorage.setItem('Boards', JSON.stringify(boards))
+    }
+    showColumn()
+    cancelBtn.click()
 })
