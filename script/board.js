c71ce170c61150999628caa9703d7315266cdfd5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-08 19:19:56 +0200

    🐛 FIX: when reload, column still on the page (due to local storage)

--- script/board.js
+++ script/board.js
@@ -4,55 +4,40 @@ const entitleButtonDiv = document.querySelector('.entitle-column')
 const entitleColumnInput = document.getElementById('entitle-column-input')
 const addBtn = document.querySelector('.add-btn')
 const cancelBtn = document.querySelector('.cancle-column-btn')
+const wrapper = document.querySelector('.column-wrapper')
 
-const boards = JSON.parse(localStorage.getItem('board') || '[]')
+const boards = JSON.parse(localStorage.getItem('Boards') || '[]')
 
 addColumnBtn.addEventListener('click', () => {
     entitleColumnInput.value = ''
     entitleColumnInput.focus()
-    entitleButtonDiv.classList.add('show')
     addColumnDiv.classList.add('hide')
-
+    entitleButtonDiv.classList.add('show')
 })
 
 cancelBtn.addEventListener('click', () => {
-    entitleButtonDiv.classList.remove('show')
     addColumnDiv.classList.remove('hide')
+    entitleButtonDiv.classList.remove('show')
 })
 
 function showColumn() {
-    boards.forEach(column => {
-        let liTag = `<div class="add-card">
-        <h3 class="column-title">${column.column}</h3>
+    document.querySelectorAll('.add-card').forEach((board) => board.remove())
+    boards.forEach((board) => {
+        let buttonTag = `<div class="add-card">
+        <h3 class="column-title">${board.column}</h3>
         <button class="add-card-btn"><i class="fa-solid fa-plus"></i> New Card</button>
-        <ul class="card-list">
-            <li class="column-card">
-                <div class="card-details">
-                    <h3 class="title">Card Title</h3>
-                    <p>Lorem ipsum, dolor sit amet consectur adipisig boli somin kol elit.
-                    </p>
-                </div>
-                <div class="line"></div>
-                <div class="bottom-details">
-                    <span class="date">June 4, 2022</span>
-                    <div class="settings"><i class="fa-solid fa-ellipsis" onclick="showMenu(this)"></i>
-                        <ul class="settings-menu">
-                            <li class="edit-btn"> <i class="fa-solid fa-pen"> </i> Edit </li>
-                            <li class="delete-btn"> <i class="fa-solid fa-trash"> </i> Delete </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-        </ul>
+
     </div>`
 
-        addColumnDiv.insertAdjacentHTML('afterend', liTag)
-
-    });
+        wrapper.insertAdjacentHTML('afterend', buttonTag)
+    })
 }
 
-addBtn.addEventListener('click', () => {
-    let columnTitle = entitleColumnInput.value;
+showColumn()
+
+addBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let columnTitle = entitleColumnInput.value
 
     if (columnTitle) {
         let boardInfo = {
@@ -61,12 +46,12 @@ addBtn.addEventListener('click', () => {
                 title: '',
                 description: '',
                 date: '',
-            }
+            },
         }
-
         boards.push(boardInfo)
         localStorage.setItem('Boards', JSON.stringify(boards))
     }
+
     showColumn()
     cancelBtn.click()
 })
