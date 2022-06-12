8c7d9185bff8c0d871d8f40e6c948581609780b5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-12 16:20:02 +0200

    💡 create drop, dragover, dragleave and drag enter function

--- JS/board.js
+++ JS/board.js
@@ -1,14 +1,44 @@
 const cards = document.querySelectorAll(".card");
+const status = document.querySelectorAll(".column")
+let draggableCard = null;
 
 cards.forEach(card => {
     card.addEventListener('dragstart', dragStart)
     card.addEventListener('dragend', dragEnd)
 })
 
+status.forEach(column => {
+    column.addEventListener('dragover', dragOver)
+    column.addEventListener('dragenter', dragEnter)
+    column.addEventListener('dragleave', dragLeave)
+    column.addEventListener('drop', dragDrop)
+
+})
+
 function dragStart() {
+    draggableCard = this;
     console.log(dragStart)
 }
 
 function dragEnd() {
-console.log(dragEnd)
+    draggableCard = null;
+    console.log(dragEnd)
+}
+
+function dragOver(event) {
+    event.preventDefault()
+}
+
+
+function dragEnter() {
+    console.log('dragover')
+}
+
+
+function dragLeave() {
+    console.log('dragover')
+}
+
+function dragDrop() {
+    console.log('dropped')
 }
