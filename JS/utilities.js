2f5803545f0bb5f30b88dbef7ed70fca6129db10
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-14 07:08:13 +0200

    remoe every thing that has to withnthe "board-page"

--- JS/utilities.js
+++ JS/utilities.js
@@ -1,8 +1,8 @@
 const introductionLink = document.querySelector('.introduction-link')
 const introductionPage = document.querySelector('.introduction')
 const navLinks = document.querySelectorAll('.item-links')
-const boardLink = document.querySelector('.boards-link')
-const boardPage = document.querySelector('.board-page')
+const taskLink = document.querySelector('.tasks-link')
+const taskPage = document.querySelector('.task-page')
 const noteLink = document.querySelector('.notes-link')
 const notePage = document.querySelector('.note-page')
 
@@ -16,16 +16,16 @@ navLinks.forEach((link) => {
 
 function showPages(link) {
   if (link === introductionLink) {
-    boardPage.classList.remove('show')
+    taskPage.classList.remove('show')
     notePage.classList.remove('show')
     introductionPage.classList.remove('hide')
-  } else if (link === boardLink) {
-    boardPage.classList.add('show')
+  } else if (link === taskLink) {
+    taskPage.classList.add('show')
     notePage.classList.remove('show')
     introductionPage.classList.add('hide')
   } else if (link === noteLink) {
     notePage.classList.add('show')
-    boardPage.classList.remove('show')
+    taskPage.classList.remove('show')
     introductionPage.classList.add('hide')
   }
 }
