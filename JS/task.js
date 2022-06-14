327e05a75e8f5612a3d10855ade0b2a839ce6466
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-14 13:08:44 +0200

    💡show todo on page

--- JS/task.js
+++ JS/task.js
@@ -1,5 +1,31 @@
 const taskInput = document.querySelector('.task-input input')
+const taskBox = document.querySelector('.task-box')
+
 let tasks = JSON.parse(localStorage.getItem('task'))
+
+function showTasks() {
+  let li = ''
+
+  if (tasks) {
+    tasks.forEach((tasks, id) => {
+      li += `<li class="task">
+                    <label for="${id}">
+                        <input type="checkbox" id="${id}">
+                        <p>${tasks.name}</p>
+                    </label>
+                    <div class="settings">
+                        <i class="fa-solid fa-ellipsis"></i>
+                        <ul class="task-menu">
+                            <li><i class="fa-solid fa-pen"></i>Edit</li>
+                            <li><i class="fa-solid fa-trash"></i>Delete</li>
+                        </ul>
+                    </div>
+                </li>`
+    })
+  }
+  taskBox.innerHTML = li
+}
+showTasks()
 
 taskInput.addEventListener('keyup', (e) => {
   let userTask = taskInput.value.trim()
